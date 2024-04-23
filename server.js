81b4edc8f9b64b2cf2eb3bdf10f30d6b7ed98819
Joakim <joakim.solvang.christiansen@gmail.com>
2024-04-23 19:13:29 +0200

    Node setup

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-// test
+const express = require('express');
+const app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello !');
+});
+
+app.listen(port, () => {
+    console.log(`Connect to localhost:${port}`);
+});
